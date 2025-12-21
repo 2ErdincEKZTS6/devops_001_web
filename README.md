db6c401440515beb41f9136eededf789546a8b6f
İSMAİL ERDİNÇ EKİZTAŞ <ismailerdince@gmail.com>
2025-12-21 16:01:18 +0300

    ilk commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DevOps Hello World 001
+
+### Hello 001 - Project
+
+Bu benim ilk DevOps Projem.
+
+### Docker'a terminalden login olmak için
+<hr>
+
+```
+docker login -u KULLANICI_ADI -p PAROLA
+```
---
+++ src/main/java/com/erdinc/Devops001WebApplication.java
@@ -0,0 +1,13 @@
+package com.erdinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops001WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops001WebApplication.class, args);
+    }
+
+}
